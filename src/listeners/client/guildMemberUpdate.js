2e61522255409f14250c2b6c342c4e03474b9de7
Alexλndre <56075082+AlexandreSama@users.noreply.github.com>
2022-02-12 14:30:52 +0100

    add example for Patou

--- src/listeners/client/guildMemberUpdate.js
+++ src/listeners/client/guildMemberUpdate.js
@@ -1,4 +1,5 @@
 const {Listener} = require('discord-akairo')
+const { MessageEmbed } = require('discord.js');
 
 class guildMemberUpdateListener extends Listener{
     constructor(){
@@ -12,8 +13,21 @@ class guildMemberUpdateListener extends Listener{
         const oldStatus = oldMember.premiumSince;
         const newStatus = newMember.premiumSince;
 
+        //Exemple a modifier
+        const embedAddBoost = new MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle('Some title')
+        .setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+        .setDescription('Some description here')
+        .addFields(
+            { name: newMember.user.tag, value: "Viens de booster le serveur !" }
+        )
+        .setImage('https://i.imgur.com/AfFp7pu.png')
+        .setTimestamp()
+        .setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+
         if (!oldStatus && newStatus) {
-            this.client.channels.cache.get('866047187535921172').send(`${newMember.user.tag} viens de booster le serveur, merci a lui !`)
+            this.client.channels.cache.get('866047187535921172').send({embeds: [embedAddBoost]})
         }
 
         if(oldStatus && !newStatus){
