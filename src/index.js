ace2bccfae73a451308ff1949ef53e8cbd657079
Cyril59310 <70776486+cyril59310@users.noreply.github.com>
2022-02-12 14:29:56 +0100

    add API status

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const EtoClient = require('./structures/EtoClient');
 const Config = require('./util/config')
+const request = require('request')
 const {
     exec
 } = require("child_process");
 const io = require('@pm2/io')
+
+setInterval(() => {
+    request("http://45.155.170.110:3001/api/push/zoftCAkwkC?msg=OK&ping=")
+    console.log('ping status page')
+  }, 2000);
 
 let client = new EtoClient({
     prefix: "+"
