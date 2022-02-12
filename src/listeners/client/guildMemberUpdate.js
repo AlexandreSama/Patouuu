b9a4f13ac1474656123f8230e0c68d81259b109f
Patouuu <serveur.khg.fr@outlook.fr>
2022-02-12 14:29:14 +0100

    modif guild member update

--- src/listeners/client/guildMemberUpdate.js
+++ src/listeners/client/guildMemberUpdate.js
@@ -13,11 +13,11 @@ class guildMemberUpdateListener extends Listener{
         const newStatus = newMember.premiumSince;
 
         if (!oldStatus && newStatus) {
-            this.client.channels.cache.get('866047187535921172').send(`${newMember.user.tag} viens de booster le serveur, merci a lui !`)
+            this.client.channels.cache.get('942048925538459688').send(`${newMember.user.tag} viens de booster le serveur, merci a lui !`)
         }
 
         if(oldStatus && !newStatus){
-            this.client.channels.cache.get('866047187535921172').send(`Oh déséspoir, ${newMember.user.tag} viens de retirer son boost !`)
+            this.client.channels.cache.get('942048925538459688').send(`Oh déséspoir, ${newMember.user.tag} viens de retirer son boost !`)
         }
     }
 }
